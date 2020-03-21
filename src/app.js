b7c54cab9206d30e1f6688c18f718b15b1fcfbe9
asharaloran96 <asharoran96@gmail.com>
2020-03-21 22:24:25 +0200

    install react wow and setup assets folder

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ import { Provider } from 'react-redux';
 import store from './store/index.js';
 import Donor from './component/donor/donor.js';
 //-----------------------------//
-// import Header from './component/header/header.js';
+import Header from './component/header/header.js';
 // import Footer from './component/footer/footer.js';
 
 export default (props) => {
   return(
     <Provider store={store}>
-      {/* <Header/> */}
-      <Donor/>
+      <Header/>
+      {/* <Donor/> */}
       {/* <Footer/> */}
     </Provider>
   );
