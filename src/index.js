124d522005811c3160452294b936b67ca6704938
Nsuliman <nsuliman88@gmail.com>
2020-03-21 02:36:30 +0200

    auth login

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './app.js';
+
+function Entry() {
+    return (
+        <App />
+    )
+} // end of Entry Function 
+
+const enrtyPoint = document.getElementById('root');
+ReactDom.render(<Entry />, enrtyPoint);
