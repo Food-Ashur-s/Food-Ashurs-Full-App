8a1e0519162b0808f18155f3e9df016f44d446d7
asharaloran96 <asharoran96@gmail.com>
2020-03-22 17:15:34 +0200

    do responsive footer

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -45,6 +45,7 @@ class Footer extends React.Component{
             <a href="#" class="fa fa-twitter"></a>
             <a href="#" class="fa fa-google"></a>
             <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-instagram"></a>
           </div>
           <div class="footer-copyright">©2020 CopyRight:
             <a href="https://food--ashurs.herokuapp.com/"> Food Ashur's</a>
