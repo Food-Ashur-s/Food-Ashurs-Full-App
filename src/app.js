d221b2b82e4b39adca48893bac7042a6af2bdf89
Nsuliman <nsuliman88@gmail.com>
2020-03-22 02:14:40 +0200

    trying with google login , almost there

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Log from './component/users/users.js'
 
 export default function App (){
   return (
     <>
-      <h1> hello</h1>
+      <h1>Hello there</h1>
+      <Log />
     </>
   );
 }
---
+++ src/component/users/users.js
@@ -0,0 +1,36 @@
+import React , { Component } from 'react';
+import GoogleLogin from 'react-google-login'
+
+export default class Login extends Component {
+
+    responseGoogle = async (response) => {
+       const userObject = {
+          username: response.w3.ofa,
+          password: 'test'
+       }
+       if(response.w3.ofa) {
+          await localStorage.setItem("user", JSON.stringify(userObject));
+          await window.location.reload();
+       } else {
+
+    }
+    console.log(response);
+    }
+    
+    render(){
+       return(
+           <div>
+              <input type="text" placeholder="username"/>
+              <input type="text" placeholder="password"/> 
+              <button>Login</button>
+              <GoogleLogin
+                clientId="729663215177-d2uq3c446ce2gfkoopbuhm4debo4crvf.apps.googleusercontent.com
+                " 
+                buttonText="Log in with Google"
+                onSuccess={this.props.responseGoogle}
+                onFailure={this.props.responseGoogleError}
+              />
+           </div>
+       )
+    }
+}
