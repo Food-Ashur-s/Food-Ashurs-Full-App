419a73a4eeccfd51195e53893a332e5e0f46cd8b
asharaloran96 <asharoran96@gmail.com>
2020-03-22 17:33:31 +0200

    edit humporger list

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -44,7 +44,7 @@ class Header extends React.Component{
     return (
       <>
         <div className='header-containar'>
-          <img src={logo} height="70px" width="200px"/>
+          <img src={logo}  className="wow fadeInLeft slower" height="60px" width="200px"/>
           <link rel="stylesheet" href="animate.min.css"></link>
           {/* <div className="wow pulse slower" data-wow-offset='50' data-wow-delay="0s" data-wow-iteration="500">
             <h1>Food Ashur's</h1>
