0053ba70d83c85c9c35eb48ae65310ccf4444215
Nsuliman <nsuliman88@gmail.com>
2020-03-23 00:37:50 +0200

    trying other ways

---
+++ src/component/users/googleComp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login-component';
+ 
+class Login extends React.Component{
+ 
+  constructor (props, context) {
+    super(props, context);
+  }
+ 
+  responseGoogle (googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    var googleId = googleUser.getId();
+    
+    console.log({ googleId });
+    console.log({accessToken: id_token});
+    //anything else you want to do(save to localStorage)...
+  }
+ 
+  render () {
+    return (
+      <div>
+        <GoogleLogin socialId="729663215177-d2uq3c446ce2gfkoopbuhm4debo4crvf.apps.googleusercontent.com"
+                     className="google-login"
+                     scope="profile"
+                     fetchBasicProfile={false}
+                     responseHandler={this.responseGoogle}
+                     buttonText="Login With Google"/>
+      </div>
+    );
+  }
+ 
+}
+ 
+export default Login;
