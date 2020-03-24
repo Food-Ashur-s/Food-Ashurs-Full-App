90ff4c489a85ad22d605b7eb6bbcd008e1a08ad7
Ayman <aymankhawaldeh8@gmail.com>
2020-03-24 18:53:22 +0200

    fixed

--- src/component/aboutus/about.js
+++ src/component/aboutus/about.js
@@ -48,9 +48,9 @@ class About  extends React.Component {
             <p>  Full-Stack web software developer, with a specialization in Javascript and a deep passion for back-end
 server and API development,
 Graduate of the Luminus Technical College University’s Advanced School of Computing, 401 Advanced Javascript Software Development program.</p>
-          <div class="button_container">
-            <a href="https://github.com/Qusai-alhanaktah" className="github" ><button className="btn">Github Link</button></a>
-            <a href="https://www.linkedin.com/in/qusai-alhanakta-081091193/" className="linked" ><button className="btn">LinkedIn Link</button></a>
+            <div class="button_container">
+              <a href="https://github.com/Qusai-alhanaktah" className="github" ><button className="btn">Github Link</button></a>
+              <a href="https://www.linkedin.com/in/qusai-alhanakta-081091193/" className="linked" ><button className="btn">LinkedIn Link</button></a>
             </div>
         
           </div>
@@ -71,9 +71,9 @@ Graduate of the Luminus Technical College University’s Advanced School of Comp
             <p>Versatile Javascript web application developer, which stems from extensive technical training and a prior background in chemical engineering.
 
 Through my strong understanding of back-end and user interface development, I aspire to work for a company where I can contribute meaningfully to modern software projects, where I work alongside a dedicated team of passionate software engineers. </p>
-          <div class="button_container">
-            <a href="https://github.com/ASHARALORAN96" className="github" ><button className="btn">Github Link</button></a>
-            <a href="https://www.linkedin.com/in/as-har-oran-%D8%A3%D8%B3%D8%AD%D8%A7%D8%B1-3a635416b/" className="linked" ><button className="btn">LinkedIn Link</button></a>
+            <div class="button_container">
+              <a href="https://github.com/ASHARALORAN96" className="github" ><button className="btn">Github Link</button></a>
+              <a href="https://www.linkedin.com/in/as-har-oran-%D8%A3%D8%B3%D8%AD%D8%A7%D8%B1-3a635416b/" className="linked" ><button className="btn">LinkedIn Link</button></a>
             </div>
           </div>
           
@@ -89,7 +89,7 @@ Through my strong understanding of back-end and user interface development, I as
 
 
         <div className="section" id="p4">
-          <span className="name">Nawal Sulaiman</span>
+          <span className="name">Nawal Suliman</span>
           <span className="border"></span>
           <div className="par">
             <p>Bachelor’s Degree in Computer Engineering from Jordan University of Science and Technology.
@@ -97,11 +97,11 @@ Javascript Software Engineer.
 Senior Customer Support Specialist.
 
 Core professional values include honesty, a hard-working nature, support and mentorship of others, and a constant desire to learn and grow. </p>
-          <div class="button_container">
-            <a href="https://github.com/orgs/Food-Ashur-s/people/Nsuliman" className="github" ><button className="btn">Github Link</button></a>
-            <a href="https://www.linkedin.com/in/nawal-suliman-sherine/" className="linked" ><button className="btn">LinkedIn Link</button></a>
+            <div class="button_container">
+              <a href="https://github.com/orgs/Food-Ashur-s/people/Nsuliman" className="github" ><button className="btn">Github Link</button></a>
+              <a href="https://www.linkedin.com/in/nawal-suliman-sherine/" className="linked" ><button className="btn">LinkedIn Link</button></a>
          
-          </div>
+            </div>
           </div>
           
  
