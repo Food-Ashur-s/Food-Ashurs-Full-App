143f8e611bf904f3784a359ab9d195987e4e00e7
Ayman <aymankhawaldeh8@gmail.com>
2020-03-24 18:52:09 +0200

    finished about us page

---
+++ src/app.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import About from './component/aboutus/about.js';
+
+
+class App extends React.Component {
+  
+  render() { 
+    return ( 
+      <React.Fragment>
+        <About />
+      </React.Fragment>
+    );
+  }
+}
+export default App;
