5811d7a7f1c074c0d9b6f70966317aadad293eec
Ayman <aymankhawaldeh8@gmail.com>
2020-03-29 17:15:59 +0300

    done

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { LoginContext } from './auth/context.js';
-import GaugeChart from 'react-gauge-chart';
 
 import cartPhoto from '../assets/profile-picture.jpg';
 import logo from '../assest/mainLogo.PNG';
@@ -18,6 +17,7 @@ import './profile.scss';
 import {When} from './if';
 import Gauge from 'react-svg-gauge';
 import ReactSpeedometer from 'react-d3-speedometer';
+import GaugeChart from 'react-gauge-chart';
 
 
 window.$ = window.jQuery = require('jquery');
@@ -77,24 +77,113 @@ handelShowUserUpdate = e => {
 
 render() {
   return (
-    <React.Fragment>
-      <main class="page__course">
+    <React.Fragment> 
+      
+      <div className='header-containar'>
+        <img src={logo}  className="wow fadeInLeft slower logo" height="65px" width="200px"/>
+        <link rel="stylesheet" href="animate.min.css"></link>
+        <div className="wow pulse slower" data-wow-offset='50' data-wow-delay="0s" data-wow-iteration="500">
+          <h1 className="food">Food Ashur's</h1>
+        </div> 
+        <div  className="open" onClick={this.handleClick}>
+          <span className="cls"></span>
+          <span>
+            <ul className="sub-menu ">
+              <li>
+                <Link to='/Food-Ashur-s/Food-Ashurs-Full-App'>Home</Link>
+              </li>
+              <li>
+                <NavLink to='/profile'>profile</NavLink>
+              </li>
+  
+              <li>
+                <a href="#resipient" title="resipient">Order</a>
+              </li>
+              <li>
+                <a href="#aboutUs" title="aboutUs">About Us</a>
+              </li>
+              <li>
+                <If condition={this.context.loggedIn}>
+                  <a onClick={this.context.logout} >Log Out!</a>
+                </If>
+                <If condition={!this.context.loggedIn}>
+                  <a onClick={this.handleSignClick}>Log In</a>
+                  {/* <button onClick={this.handleSignClick} className="signForm-button" >
+      SignIn</button> */}
+                </If>
+                {/* <a href="#setting" title="setting">Log Out</a> */}
+              </li>
+            </ul>
+          </span>
+          <span className="cls"></span>
+        </div>
+      </div> 
+      <main class="page__course">  
         <div class="container">
 
 
           <div class="course-content">
 
-            <h1>Managing Work at Height</h1>
+
+            <h1 className="namm">{this.state.user.username } Profile</h1>
+            <div class="content-block">
+              <span class="rat">rating</span>
+              <span class="stars">★★★★☆</span>
+              <div className="cir">
+                <GaugeChart id="gauge-chart2" 
+                  nrOfLevels={20} 
+                  percent={0.86}
+                  // colors={['#0b7685dc' , 'yellow' , 'yellow']}
+                />
+
+                <GaugeChart id="gauge-chart3" 
+                  nrOfLevels={30} 
+                  colors={['#0b7685dc', '#FFC371']} 
+                  arcWidth={0.3} 
+                  percent={0.37} 
+
+                  
+                />
+
+
+              </div>
+            </div>
+           
 
             <div class="content-block">
-              <span class="label">HoundRank</span>
-              <span class="stars">★★★☆☆</span>
-            </div>
+              {!this.showUserUpdate && (<button onClick={this.handelShowUserUpdate} class="btn11">Update</button>)}
 
-            <div class="content-block">
-              <a class="button button--booking">Book Course</a>
-              <a class="button button--download">Download PDF</a>
+              {/* <a class="button button--booking">Book Course</a> */}
+              {/* <a class="button button--download">Download PDF</a> */}
+            
             </div>
+            <When condition={this.state.showUserUpdate}>
+              <Model title='user update' close={this.handelShowUserUpdate}>
+                <div className="user-updated">
+                  <form onSubmit={ e =>{
+                    localStorage.setItem('newUser', JSON.stringify(this.state.updated));
+                    this.setState({updateList: localStorage.setItem('newUser', JSON.stringify(this.state.updated))});
+                    this.setState({showUserUpdate: localStorage.setItem('showNewUser', JSON.stringify(true))});
+                  }} value={this.updated}>
+                    <label> Your Name is:
+                      <input type='text' name='username' placeholder='type your name' defaultValue={this.state.user.username} onChange={this.handleChange} required />
+                      <br/>
+                    </label>
+                    <label> Your role is:
+                      <input type='text' name='capabilities' placeholder='type your name' defaultValue={this.state.user.capabilities} onChange={this.handleChange} required />
+                      <br/>
+                    </label>
+                    <label> Your Email is:
+                      <input type='text' name='userEamil' placeholder='type your name' defaultValue={this.state.user.userEamil} onChange={this.handleChange} required />
+                      <br/>
+                    </label>
+                    <button >Submit</button>
+                  </form>
+                </div>
+              </Model>
+            </When>
+            
+            
 
             <div class="content-block">
               <ul id="tabs">
@@ -106,10 +195,8 @@ render() {
 
               <ul id="tab">
                 <li class="active">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Moatibus amet quisquam accus, porro, alias sed distinctio. necessitatibus amet quisquam accusamus minus rederit cum dolores ab ratione, porro, alias sed distinctio.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing asd. Modi quo, culpa susc ipsum assumenda odio doloribus necessitatibus amequam porro, alias sed distinct. necessitatibus quisquam accusamus minus reprehenderit cum dolores ab ratione, porro,as
-              sed distinctio.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi quo, culpa susc ipsum assumes sed distinct. necessitatibuo joij oij bhuohb ouyone, porro,as sed distinctio.</p>
+                  <p>{this.context.user.username}  is one of the distinguished users of this website and the people who contribute to spreading goodness on Earth.</p>
+                
                 </li>
 
 
@@ -117,36 +204,56 @@ render() {
             </div>
           </div>
           <aside class="courseSidebar">
-            <img className="person" src={this.state.image } />
-            <div className="changephoto">
-            
-              {this.state.showUpload && (<input type="file" name="myImage" className="bt" onChange={this.onImageChange} />)}
-              {!this.state.showUpload && (<button onClick={this.showingUpload}>Edit My Photo</button>)}
+            <div className="masthead">
+              <img src={this.state.image} className="avatar"/>
             </div>
 
+        
+            <div className="change">
+              {this.state.showUpload && (<input type="file" name="myImage" class="btn1" onChange={this.onImageChange} />)}
+              {!this.state.showUpload && (<button class="btnz" onClick={this.showingUpload}>Edit My Photo</button>)}
+            </div>
 
-            <ul class="courseStats">
-              <li>
-                <span class="label courseStats__label">Provider</span>
-                <span class="courseStats__divider"></span>
-                <span class="courseStats__data">Leading Edge Safety</span>
-              </li>
-              <li>
-                <span class="label courseStats__label">Duration</span>
-                <span class="courseStats__divider"></span>
-                <span class="courseStats__data">Half Day</span>
-              </li>
-              <li>
-                <span class="label courseStats__label">Suitable For</span>
-                <span class="courseStats__divider"></span>
-                <span class="courseStats__data">Managers</span>
-              </li>
-              <li>
-                <span class="label courseStats__label">Subject</span>
-                <span class="courseStats__divider"></span>
-                <span class="courseStats__data">Working At Height</span>
-              </li>
-            </ul>
+            {!this.state.showUpdateList && (
+              <ul class="courseStats">
+                <li>
+                  <span class="label courseStats__label" >User name</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.username }</span>
+                </li>
+                <li>
+                  <span class="label courseStats__label">TYpe</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.capabilities }</span>
+                </li>
+                <li>
+                  <span class="label courseStats__label">E-mail</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.userEamil }</span>
+                </li>
+               
+              </ul>
+            )}
+            {this.state.showUpdateList && (
+              <ul class="courseStats">
+                <li>
+                  <span class="label courseStats__label" >User name</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.username }</span>
+                </li>
+                <li>
+                  <span class="label courseStats__label">TYpe</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.capabilities }</span>
+                </li>
+                <li>
+                  <span class="label courseStats__label">E-mail</span>
+                  <span class="courseStats__divider"></span>
+                  <span class="courseStats__data">{this.state.user.userEamil }</span>
+                </li>
+               
+              </ul>
+            )}
           </aside>
         </div>
       </main>
@@ -194,7 +301,16 @@ export default Profile;
 //     </ul>
 //   )}
 
-//   {!this.showUserUpdate && (<button onClick={this.handelShowUserUpdate}>Updae</button>)}
+
+
+
+
+
+
+
+
+
+//  {!this.showUserUpdate && (<button onClick={this.handelShowUserUpdate}>Updae</button>)}
 
 //   <When condition={this.state.showUserUpdate}>
 //     <Model title='user update' close={this.handelShowUserUpdate}>
@@ -229,23 +345,21 @@ export default Profile;
 
 
 // <div>
-// <GaugeChart id="gauge-chart2" 
-//   nrOfLevels={20} 
-//   percent={0.86}
-//   needleColor	={"blue"}
-// />
+//   <GaugeChart id="gauge-chart2" 
+//     nrOfLevels={20} 
+//     percent={0.86}
+//     needleColor	={'blue'}
+//   />
 
-// <GaugeChart id="gauge-chart3" 
-//   nrOfLevels={30} 
-//   colors={['#FF5F6D', '#FFC371']} 
-//   arcWidth={0.3} 
-//   percent={0.37} 
-// />
+//   <GaugeChart id="gauge-chart3" 
+//     nrOfLevels={30} 
+//     colors={['#FF5F6D', '#FFC371']} 
+//     arcWidth={0.3} 
+//     percent={0.37} 
+//   />
 
-// {/* <ReactSpeedometer />
 
-// <Gauge value={this.state.value} width={400} height={320} label="This is my Gauge" /> */}
-// </div>
+// </div>;
 
 
 
