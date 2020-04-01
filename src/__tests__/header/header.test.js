1053c6b6caf510f1990ebc9772d3f50da6a09071
Nsuliman <nsuliman88@gmail.com>
2020-04-01 22:45:01 +0300

    tests added

--- src/__tests__/header/header.test.js
+++ src/__tests__/header/header.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-import Donor from '../../component/donor/donor.js';
+import Header from '../../component/header/header.js';
 
 
 describe ('<ToDoList />' , () => {
 
   it(' It Should be exists in our applications ', () => {
     // virtual 
-    let app = shallow(<Donor />);
+    let app = shallow(<Header />);
     // find the tag which already includes in our app 
-    expect(app.find('p').exists()).toBeTruthy();
+    expect(app.find('li').exists()).toBeTruthy();
   }); // end of it test 
 
   it(' Renders correctly ' , () => {
-    const tree = renderer.create(<Donor />).toJSON();
+    const tree = renderer.create(<Header />).toJSON();
     expect(tree).toMatchSnapshot();
   }); // end of it test for render 
 
